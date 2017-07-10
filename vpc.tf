2fcca9d0400aa8e06289ae2c920bbb0e98f85a08
Matthew Ceroni <matthew.ceroni@smarsh.com>
2017-07-10 10:59:57 -0700

    Define ingress/egress rules as aws_security_group_rule vs inline.
    This allows rules to be added outside of terraform (ie: kubernetes)
    without being over-written.

--- vpc.tf
+++ vpc.tf
@@ -10,29 +10,45 @@ resource "aws_security_group" "inbound-outbound" {
   /* link to the correct VPC */
   vpc_id = "${var.vpc_id}"
 
-  /*
-    Define ingress rules
-  */
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
-
-  /*
-    Define egress rules
-  */
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
-
   tags {
     builtWidth        = "terraform"
     KubernetesCluster = "${var.name}"
     Name              = "kubernetes-worker-${var.name}"
   }
+}
+
+/*
+  Define ingress rules
+
+  Has to be done outside the group creation as Kubernetes will add/remove
+  rules as part of provisioning and these would get over-written by terraform
+*/
+resource "aws_security_group_rule" "allow_all" {
+
+  type = "ingress"
+
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+
+  cidr_blocks = [ "0.0.0.0/0" ]
+
+  security_group_id = "${aws_security_group.inbound-outbound.id}"
+
+}
+
+/*
+  Define egress rules
+*/
+resource "aws_security_group_rule" "allow_all-outbound" {
+
+  type = "egress"
+
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+
+  cidr_blocks = [ "0.0.0.0/0" ]
+
+  security_group_id = "${aws_security_group.inbound-outbound.id}"
 }
